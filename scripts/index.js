d18590b783660c74cf61ea6494a89bca9ad11d97
cbalaji1997 <balajichavan093@gmail.com>
2022-05-02 12:23:29 +0530

    unit3c

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+// Ude Import export (MANDATORY)
+// Onclicking store the news in local storage with key "news" so that you can access that on news.html page
+
+
+document.getElementById("navbar").innerHTML=navbar();
+
+
+import sidebar from"../components/sidebar.js";
+
+document.getElementById("sidebar").innerHTML=sidebar()
